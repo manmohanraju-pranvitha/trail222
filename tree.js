4193cf99e08720b0655ad6bbb281b3ddca81ad99
manmohanraju-pranvitha <73023267+manmohanraju-pranvitha@users.noreply.github.com>
2020-11-20 13:40:26 +0530

    Add files via upload

---
+++ tree.js
@@ -0,0 +1,20 @@
+class Tree{
+    constructor(x,y,width,height){
+        var option={
+            isStatic:true
+            
+        }
+        this.body=Bodies.rectangle(x,y,width,height,option);
+        this.width=width;
+        this.height=height;
+        this.image=loadImage("tree.png");
+        
+        World.add(world,this.body);
+    }
+      display(){
+        var pos=this.body.position;
+        imageMode(CENTER);
+        fill("black");
+        image(this.image,pos.x,pos.y,this.width,this.height);
+    }
+}
